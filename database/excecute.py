3acf13cccd2155dbec76769345c263dc93bf0b03
sgowdaks <shivanigowdaks@gmail.com>
2022-11-28 14:30:37 -0800

    delete updated

--- database/excecute.py
+++ database/excecute.py
@@ -42,6 +42,10 @@ class execute:
         first_part = query_brackets[0]
         second_part = query_brackets[1]
         tabel_name = first_part.split(" ")[2]
+        extra_tabel_name = tabel_name + "_extra"
+        extra = first_part.split(" ")
+        extra = extra[:len(extra)-2]
+        extra = " ".join(extra) + " " + extra_tabel_name
         second_part = second_part.strip(")")
         arr = second_part.split(" ")
         arr = second_part.split(" ")
@@ -51,14 +55,16 @@ class execute:
             new = "," + new_col
             arr.append(new)
             arr = " ".join(arr)
-            arr = first_part + "(" + arr + ")"
-            self.con.execute(arr)
+            arr_ = first_part + "(" + arr + ")"
+            self.con.execute(arr_)
+            arr_ = extra + "(" + arr + ")"
+            self.con.execute(extra + "(" + arr + ")")
             #create_extra_database(arr)
         else:
             print("handel the error")
         # self.con.close()
 
-    def insert(self, query):
+    def insert(self, query, bo = False):
         query = query.split("(")
         tabel_name = query[0].split(" ")[2]
         second = query[1].strip(")").strip('"').split(",")
@@ -77,11 +83,11 @@ class execute:
                 tup.append(eval(second[i]))
             if i == n-1:
                 extra = second[i]
-       
-        question += ", ?"
-        today = date.today()
-        d1 = today.strftime("%d/%m/%Y")
-        tup.append(extra +":" + d1 + " ,")
+        if bo == False:
+            question += ", ?"
+            today = date.today()
+            d1 = today.strftime("%d/%m/%Y")
+            tup.append(extra +":" + d1 + " ,")
         str_ = "INSERT INTO " + tabel_name + " VALUES (" + question + ")" 
         self.con.execute(str_, tuple(tup))
         self.cur.commit()
@@ -179,22 +185,36 @@ class execute:
         dicti1, keys1, list1 = make_dicti(dicti1, where_part, list1 = [])
 
         q = "SELECT * FROM " + tabel_name + " WHERE " + keys1
-        #insert_into_extra_database(q)
+        self.insert_into_extra_database(q, list1, tabel_name)
 
         q = "DELETE FROM " + tabel_name + " WHERE " + keys1 
         cur.execute(q, tuple(list1))
         self.cur.commit()
         self.cur.close()
 
-
+    def insert_into_extra_database(self, q, vals, tabel_name):
+        extra_tabel_name = tabel_name + "_extra"
+        cur.execute(q, tuple(vals))
+        con.commit()
+        rows = cur.fetchall()
+        one = cur.fetchone()
+        query = "INSERT INTO " + extra_tabel_name + " VALUES " 
+        list1 = []
+        for row in rows:
+            for i, val in enumerate(row):
+                list1.append(val.strip(" "))
+            list1[-1] = list1[-1].strip(",")
+            query = query + str(tuple(list1))
+            self.insert(query, True)
+            
     # def view(self, query):
         
-# k = execute("CREATE TABLE users (userId text primary key, name text, email text, salary text save)", cur, con)
-#k = execute("INSERT INTO users VALUES (1, 'sherry', 'sks@lion.lmu.edu', 1000)", cur, con)
-#k = execute("INSERT INTO users VALUES (2, 'shivani', 's@lion.lmu.edu', 2000)", cur, con)        
+k = execute("CREATE TABLE users (userId text primary key, name text, email text, salary text save)", cur, con)
+k = execute("INSERT INTO users VALUES (1, 'sherry', 'sks@lion.lmu.edu', 1000)", cur, con)
+k = execute("INSERT INTO users VALUES (2, 'shivani', 's@lion.lmu.edu', 2000)", cur, con)        
 #k = execute("UPDATE INTO users SET salary = 2000, name = 'TG' WHERE email = 'sks@lion.lmu.edu'", cur, con)
 #k = execute("UPDATE INTO users SET salary = 3000 WHERE email = 'sks@lion.lmu.edu'", cur, con)
-#k = execute("DELETE FROM users WHERE email = 'sks@lion.lmu.edu'", cur, con) 
+k = execute("DELETE FROM users WHERE email = 'sks@lion.lmu.edu'", cur, con) 
 
 
 
